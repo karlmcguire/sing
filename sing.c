069a2bdc8f5e16eb3c077b0f7b00c3d30309769d
Karl McGuire <karl@karlmcguire.com>
2017-12-13 10:06:31 -0500

    ignore object file, idiomatic c stuff

--- sing.c
+++ sing.c
@@ -4,12 +4,14 @@
 
 #include "sing.h"
 
-void sing_init(struct sing_buffer *b) { 
+void
+sing_init(struct sing_buffer *b) { 
 	b->tail = SING_CAP;
 	b->head = SING_CAP; 
 }
 
-int sing_push(struct sing_buffer *b, char *s) {
+int
+sing_push(struct sing_buffer *b, char *s) {
 	if(sing_full(b))
 		return 0;
 
@@ -30,31 +32,36 @@ int sing_push(struct sing_buffer *b, char *s) {
 	return 1;
 }
 
-char *sing_shift(struct sing_buffer *b) { 
+char
+*sing_shift(struct sing_buffer *b) { 
 	if(sing_empty(b))
 		return "";
 
 	return b->array[(b->head++) & SING_LEN]; 
 }
 
-int sing_empty(struct sing_buffer *b) {
+int
+sing_empty(struct sing_buffer *b) {
 	return b->head == b->tail;
 }
 
-int sing_full(struct sing_buffer *b) {
+int
+sing_full(struct sing_buffer *b) {
 	return sing_size(b) == SING_CAP;
 }
 
-uint32_t sing_size(struct sing_buffer *b) { 
+uint32_t
+sing_size(struct sing_buffer *b) { 
 	return b->tail - b->head; 
 }
 
 /*
-int main() {
+int 
+main() {
 	struct sing_buffer b;
 	sing_init(&b);
 
 	sing_push(&b, "karl");
-	printf("%s\n", sing_shift(&b));	
+	printf("%s\n", sing_shift(&b));	// will output "karl"
 }
 */
