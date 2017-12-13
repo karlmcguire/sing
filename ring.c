e30014327c0e1a1ffcc915375e6a341c8e7f183c
Karl McGuire <karl@karlmcguire.com>
2017-12-12 20:48:05 -0500

    safety checks

--- ring.c
+++ ring.c
@@ -4,8 +4,8 @@
 #include <string.h>
 
 // CAP must be a power of 2 (using uint32_t overflow)
-#define CAP 4
-#define LEN CAP - 1
+#define CAP (uint32_t)4
+#define LEN CAP - (uint32_t)1
 
 struct ring {
 	uint32_t head;
@@ -13,8 +13,26 @@ struct ring {
 	char *array[CAP];
 };
 
+/* size returns the number of unread objects in the ring buffer */
+uint32_t size(struct ring *r) { 
+	return r->tail - r->head; 
+}
+
+/* empty returns 1 if the ring buffer is empty, 0 if not */
+int empty(struct ring *r) {
+	return r->head == r->tail;
+}
+
+/* full returns 1 if the ring buffer is full, 0 if not */
+int full(struct ring *r) {
+	return size(r) == CAP;
+}
+
 /* push adds an element to the tail of the ring buffer */
-void push(struct ring *r, char *v) {
+int push(struct ring *r, char *v) {
+	if(full(r))
+		return 0;
+
 	// get next index
 	int i = (r->tail++) & LEN;
 	// get length of string
@@ -28,34 +46,28 @@ void push(struct ring *r, char *v) {
 	r->array[i] = malloc(l);
 	// copy string
 	strncpy(r->array[i], v, l);
+
+	return 1;
 }
 
 /* shift returns the element from the head of the ring buffer */
 char *shift(struct ring *r) { 
+	if(empty(r))
+		return "";
+
 	return r->array[(r->head++) & LEN]; 
 }
 
 /* init sets up the ring tail field for pushing/shifting */
 void init(struct ring *r) { 
-	r->tail = (uint32_t)CAP; 
-	r->head = (uint32_t)CAP;
+	r->tail = CAP;
+	r->head = CAP; 
 }
-
-uint32_t size(struct ring *r) { return r->tail - r->head; }
-int empty(struct ring *r) { return r->head == r->tail;	}
-int full(struct ring *r) { return size(r) == (uint32_t)CAP; }
 
 int main() {
 	struct ring r;
 	init(&r);
 
 	push(&r, "karl");
-	push(&r, "noah");
-	push(&r, "finn");
-	push(&r, "max");
-
-	printf("%s\n", shift(&r));
-	printf("%s\n", shift(&r));
-	printf("%s\n", shift(&r));
-	printf("%s\n", shift(&r));
+	printf("%s\n", shift(&r));	
 }
